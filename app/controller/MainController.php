33d2d4921bb9ad982cf83e1e3d2fc51ff5458f27
korocheponyal <105550819+korocheponyal@users.noreply.github.com>
2022-12-25 15:52:58 +0400

    test

--- app/controller/MainController.php
+++ app/controller/MainController.php
@@ -15,16 +15,17 @@ use app\model\Main;
 class MainController extends AppController
 {
 //    public $layout = 'default';
-    public function indexAction(){
+    public function indexAction()
+    {
 //    $this->layout = false;
 //    $this->view = 'Test';
-    $model = new Main();
-       $name = 'ruslan';
-       $create_table = $model->query("CREATE TABLE `posts` ( `test1` int(11) NOT NULL, `test2` int(11) NOT NULL, `test3` int(11) NOT NULL, `test4` int(11) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
-       $insert_data = $model->query("INSERT INTO `posts` (`test1`, `test2`, `test3`, `test4`) VALUES (1, 2, 3, 4);");
-       $get_res = $model->findAll();
-       d($get_res);
-       $this->set(compact('name'));
+        $model = new Main();
+        $name = 'ruslan';
+        $create_table = $model->query("CREATE TABLE `posts` ( `test1` int(11) NOT NULL, `test2` int(11) NOT NULL, `test3` int(11) NOT NULL, `test4` int(11) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
+        $insert_data = $model->query("INSERT INTO `posts` (`test1`, `test2`, `test3`, `test4`) VALUES (1, 2, 3, 4);");
+        $get_res = $model->findAll();
+        d($get_res);
+        $this->set(compact('name'));
 
        
     }
