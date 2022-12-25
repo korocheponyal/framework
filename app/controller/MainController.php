c383c6003a0f0409ef4d35ed694a4f6d18777909
korocheponyal <105550819+korocheponyal@users.noreply.github.com>
2022-12-25 16:02:56 +0400

    Revert "test fatal error"
    This reverts commit 8bfb543335455de9bc04d98fb9ba379f235b54f9.

--- app/controller/MainController.php
+++ app/controller/MainController.php
@@ -21,6 +21,15 @@ class MainController extends AppController
 //    $this->view = 'Test';
         $model = new Main();
         $name = 'ruslan';
-        $create_t''
+        $create_table = $model->query("CREATE TABLE `posts` ( `test1` int(11) NOT NULL, `test2` int(11) NOT NULL, `test3` int(11) NOT NULL, `test4` int(11) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
+        $insert_data = $model->query("INSERT INTO `posts` (`test1`, `test2`, `test3`, `test4`) VALUES (1, 2, 3, 4);");
+        $get_res = $model->findAll();
+        d($get_res);
+        $this->set(compact('name'));
+
+       
+    }
+
+}
 
 
