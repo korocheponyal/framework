c22a496fdcf1032c3508f1c276620dd07c1988da
korocheponyal <105550819+korocheponyal@users.noreply.github.com>
2022-12-25 15:39:08 +0400

    CODe style

--- vendor/fw/core/Router.php
+++ vendor/fw/core/Router.php
@@ -6,28 +6,34 @@ class Router
 {
     protected static $routes = [];
     protected static $route = [];
-    public static function add($regexp,$route = []){
+
+    public static function add($regexp, $route = [])
+    {
         self::$routes[$regexp] = $route;
     }
-    public static function getRoutes(){
+
+    public static function getRoutes()
+    {
         return self::$routes;
     }
-    private static function matchRoute($url){
-        foreach(self::$routes as $pattern=>$route){
-            if(preg_match("#$pattern#i",$url,$matches)){
-                foreach ($matches as $k=>$v){
-                    if(is_string($k)){
+
+    private static function matchRoute($url)
+    {
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match("#$pattern#i", $url, $matches)) {
+                foreach ($matches as $k => $v) {
+                    if (is_string($k)) {
                         $route[$k] = $v;
                     }
                 }
-                if(!isset($route['action'])){
+                if (!isset($route['action'])) {
                     $route['action'] = 'index';
                 }
                 $route['controller'] = self::upperCamelCase($route['controller']);
                 self::$route = $route;
                 return true;
             }
-           
+
         }
         return false;
     }
@@ -37,42 +43,48 @@ class Router
      * @param  string $url input URL
      * @return void
      */
-    public static function dispatch($url){
+    public static function dispatch($url)
+    {
         $url = self::rewoveQueryString($url);
 //        d($url);
-        if(self::matchRoute($url)){
+        if (self::matchRoute($url)) {
             $controller = 'app\controller\\' . self::$route['controller'] . 'Controller';
-            if(class_exists($controller)){
+            if (class_exists($controller)) {
                 $cObj = new $controller(self::$route);
                 $action = self::lowerCamelCase(self::$route['action']) . 'Action';
-              if(method_exists($cObj,$action)){
-                  $cObj->$action();
-                  $cObj->getView();
-              }else {
-                  echo "$controller::$action <div style='color: red;'><b>not found</b></div>";
-              }
-            } else{
+                if (method_exists($cObj, $action)) {
+                    $cObj->$action();
+                    $cObj->getView();
+                } else {
+                    echo "$controller::$action <div style='color: red;'><b>not found</b></div>";
+                }
+            } else {
                 echo "$controller <div style='color: red;'><b>not found</b></div>";
             }
-        }else{
+        } else {
             http_response_code(404);
             include '404.php';
         }
     }
-    protected static function upperCamelCase($name){
-         return str_replace(' ','',ucwords(str_replace('-',' ',$name)));
+
+    protected static function upperCamelCase($name)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
 
     }
-    protected static function lowerCamelCase($name){
+
+    protected static function lowerCamelCase($name)
+    {
         return lcfirst(self::upperCamelCase($name));
     }
 
-    protected static function rewoveQueryString($url){
-        if($url){
+    protected static function rewoveQueryString($url)
+    {
+        if ($url) {
             $params = explode('&', $url, 2);
-            if(!strpos($params['0'],'=')){
-                return rtrim($params['0'],'/');
-            }else{
+            if (!strpos($params['0'], '=')) {
+                return rtrim($params['0'], '/');
+            } else {
                 return '';
             }
         }
